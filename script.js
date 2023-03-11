0bddf5fcb8050a19ef7950bc857f0effbdccb2a3
Chaiyaphon Y <124662695+minusszx@users.noreply.github.com>
2023-03-11 10:32:10 +0000

    commited

---
+++ script.js
@@ -0,0 +1,17 @@
+////// Question 1
+
+
+
+
+////// Question 2 
+function calculateAge(birthYear,futureYear){
+    const ageInFutureYear = futureYear - birthYear;
+    
+    console.log(`You will be either ${ageInFutureYear - 1} or ${ageInFutureYear} in the year ${futureYear}.`);
+}
+
+calculateAge(1999,2028);
+
+
+////// Question 3 
+
